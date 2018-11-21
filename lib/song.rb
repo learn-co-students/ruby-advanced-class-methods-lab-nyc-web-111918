791118a9da2b147820642f9a507ba4b4fd01f984
Tonyjlum <tonyjlum@gmail.com>
2018-11-21 17:43:34 -0500

    song class without init

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,57 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name).nil?
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.map.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(file_name)
+    artist, song = file_name[0..-5].split(" - ")
+    new_song = Song.new
+    new_song.name, new_song.artist_name = song, artist
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new_from_filename(file_name)
+    new_song.save
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
