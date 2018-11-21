cacc9905de2996ad9f021cc862b9d5eb580a972f
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-21 09:01:12 -0500

    finished song class

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.each do |tune|
+      if tune.name == song_name
+        return tune
+      end
+    end
+    nil
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_words = filename.split(" - ")
+    artist_name = song_words[0]
+    song_name = song_words[1][0...-4]
+    song = self.create
+    song.artist_name = artist_name
+    song.name = song_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_words = filename.split(" - ")
+    artist_name = song_words[0]
+    song_name = song_words[1][0...-4]
+    song = self.create
+    song.artist_name = artist_name
+    song.name = song_name
+    song.save
+  end
+
+  def self.destroy_all
+    @@all.length.times do |song|
+      @@all.pop
+    end
+  end
 end
